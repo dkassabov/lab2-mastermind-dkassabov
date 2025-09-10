c4241adf2cf139625f308f27c266953100d183ec
Denis Kassabov <kasab995@gmail.com>
2025-09-09 22:12:41 -0400

    Main logic

--- src/mastermind/MasterMind.java
+++ src/mastermind/MasterMind.java
@@ -1,5 +1,7 @@
 package mastermind;
 
+import java.util.Arrays;
+
 /**
  * The main class for the Mastermind game.  The secret code can be provided
  * as the command line argument, otherwise it will be randomly generated.
@@ -17,6 +19,16 @@ public class MasterMind {
     public final static int MAX_CODE_DIGIT = 8;
     /** the number of turns in the game */
     private final static int MAX_TURNS = 10;
+
+    private CodeMaker codeMaker;
+    private CodeBreaker codeBreaker;
+    private Guesses guesses;
+
+    public MasterMind(String secretCode) {
+        codeMaker = new CodeMaker(secretCode);
+        codeBreaker = new CodeBreaker();
+        guesses = new Guesses();
+    }
 
     /**
      * Check that the digits in a code are all in the valid range.
@@ -40,10 +52,67 @@ public class MasterMind {
      * @param code the code to check
      * @rit.pre code is of the correct length
      * @rit.pre code has all digits in valid range
-     * @return whether the code has no duplicates (true) or does have duplicates (false)
+     * @return whether the code has no duplicates (true) or does haveduplicates (false)
      */
     public static boolean codeNoDuplicates(String code) {
         return code.length() == code.chars().distinct().count();
+    }
+
+    public void playGame() {
+
+        boolean gameWon = false;
+        boolean quit = false;
+        int turns = 1;
+        Guess guess;
+
+        System.out.println("Welcome to MasterMind!");
+        System.out.println("Enter your 4-digit guesses (q to quit)");
+
+        // Until the game is either won or the number of turns has been exhausted
+        while (!gameWon && turns <= MAX_TURNS) {
+            // Get the next validly formatted Guess from the CodeBreaker
+            guess = codeBreaker.getGuess();
+            if (guess == null) {
+                quit = true;
+                System.out.println("Quitting...");
+                break;
+            }
+
+            // Pass the Guess to the CodeMaker for checking
+            codeMaker.checkGuess(guess);
+
+            // Add the Guess to the Guesses and display them
+            guesses.addGuess(guess);
+            guesses.displayGuesses();
+
+            // Adjust the CodeBreaker's score based on this game
+            codeBreaker.increaseScore(guess.getCorrectPositions() * 2
+                    + guess.getWrongPositions());
+
+            // Check if the CodeMaker's secret code has been cracked or not
+            // (also based on the guess)
+            if (guess.getCorrectPositions() == SECRET_CODE_LENGTH) {
+                gameWon = true;
+            }
+            else turns++;
+
+        }
+
+        if (gameWon) {
+            System.out.println("You won!");
+        }
+        else if (turns > MAX_TURNS) {
+            System.out.println("You lost! The secret code was: "
+                    + codeMaker.getSecretCode());
+        }
+
+        if (!quit) {
+            if (turns > MAX_TURNS) {
+                turns--;
+            }
+            System.out.println("Code breaker score: "
+                    + (double) codeBreaker.getScore() / turns);
+        }
     }
 
     /**
@@ -73,6 +142,9 @@ public class MasterMind {
                 return;
             }
         }
+
         // TODO - Everything is okay to play the game if we got here
+        MasterMind masterMind = new MasterMind(args[0]);
+        masterMind.playGame();
     }
 }
